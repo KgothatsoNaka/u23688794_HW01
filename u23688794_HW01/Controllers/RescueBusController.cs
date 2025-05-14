74c4e1f794bb225d670a5bc91379cb07cc458605
KgothatsoNaka <u23688794@tuks.co.za>
2025-05-14 18:14:36 +0200

    First push of info

---
+++ u23688794_HW01/Controllers/RescueBusController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace u23688794_HW01.Controllers
+{
+    public class RescueBusController : Controller
+    {
+        // GET: RescueBus
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
