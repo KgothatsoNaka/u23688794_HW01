f2b74f120378a2fcb991922bb30d11bde81e1d92
KgothatsoNaka <u23688794@tuks.co.za>
2025-05-14 17:49:16 +0200

    Add project files.

---
+++ u23688794_HW01/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace u23688794_HW01
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ u23688794_HW01/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="u23688794_HW01.MvcApplication" Language="C#" %>
